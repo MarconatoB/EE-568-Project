bb950b4f43272cdf5c7cdcf186ca5eb76ebc947b
Bastien Marconato <bastien.marconato@icloud.com>
2024-05-21 18:47:29 +0200

    ouais ouais ouais

--- plot_utils.py
+++ plot_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
 
 def aggregate_over_seeds(results):
     max_timesteps = np.max([len(seed_data[0]) for seed_data in results])
@@ -39,20 +40,26 @@ def aggregate_over_seeds(results):
             except IndexError:
                 pass
 
-    mean_rewards_t = np.nanmean(reward_agg, axis=1)
-    mean_rewards_ep = np.nanmean(episodes_agg, axis=1)
+
+    #mean_rewards_t = np.nanmean(reward_agg, axis=1)
+    #mean_rewards_ep = np.nanmean(episodes_agg, axis=1)
     mean_critic_loss = np.nanmean(critic_agg, axis=1)
     mean_actor_loss = np.nanmean(actor_agg, axis=1)
 
-    return mean_rewards_t, mean_rewards_ep, mean_critic_loss, mean_actor_loss
+    return reward_agg, episodes_agg, mean_critic_loss, mean_actor_loss
 
 def plot_results(results, env_name: str):
     env_result = results[env_name]
     
     if isinstance(env_result, list):
-        rewards_t, episodes_reward, critic_losses, actor_losses = aggregate_over_seeds(env_result)
+        reward_agg, episodes_agg, critic_losses, actor_losses = aggregate_over_seeds(env_result)
+        rewards_t = np.nanmean(reward_agg, axis=1)
+        rewards_ep = np.nanmean(episodes_agg, axis=1)
+
     else:
-        rewards_t, episodes_reward, critic_losses, actor_losses = results[env_name]
+        rewards_t, rewards_ep, critic_losses, actor_losses = results[env_name]
+        rewards_t = np.array(rewards_t)
+        rewards_ep = np.array(rewards_ep)
 
     fig, ax1 = plt.subplots()
     critic_plot = ax1.plot(critic_losses, label='critic', color='C0')
@@ -66,10 +73,50 @@ def plot_results(results, env_name: str):
     plt.show()
 
     plt.figure(figsize=(20,6))
-    episodes = np.arange(0, len(episodes_reward))
-    plt.plot(episodes_reward, '.-', label=env_name, color='C0')
-    plt.fill_between(episodes, episodes_reward, color='C0', alpha=0.2)
+    episodes = np.arange(0, rewards_ep.shape[0])
+    plt.plot(rewards_ep, '.-', label=env_name, color='C0')
+    plt.fill_between(episodes, rewards_ep, color='C0', alpha=0.2)
+
     plt.xlabel("Episode")
     plt.ylabel("Total episode reward")
     plt.legend(loc="lower right")
+
+
     plt.show()
+
+if __name__ == '__main__':
+    continuous_envs = ["MountainCarContinuous-v0", "Pendulum-v1"]
+    for env_name in continuous_envs:
+        plt.figure(figsize=(20,6))
+
+        with open(f"sac_{env_name}.obj", 'rb') as f:
+            sac_result = pickle.load(f)
+            
+            reward_agg, episodes_agg, critic_losses, actor_losses = aggregate_over_seeds(sac_result)
+
+            episodes = np.arange(0, episodes_agg.shape[0])
+            plt.plot(np.nanmean(episodes_agg, axis=1), '.-', label='SAC', color='C0')
+            #plt.fill_between(episodes, episodes_reward, color='C0', alpha=0.2)
+            plt.fill_between(
+                episodes,
+                np.nanmin(episodes_agg, axis=1), np.nanmax(episodes_agg, axis=1),
+                color='C0', alpha=0.1)
+
+        with open(f"td3_{env_name}.obj", 'rb') as f:
+            td3_result = pickle.load(f)
+
+            reward_agg, episodes_agg, critic_losses, actor_losses = aggregate_over_seeds(td3_result)
+
+            episodes = np.arange(0, episodes_agg.shape[0])
+            plt.plot(np.nanmean(episodes_agg, axis=1), '.-', label='TD3', color='C1')
+            #plt.fill_between(episodes, episodes_reward, color='C0', alpha=0.2)
+            plt.fill_between(
+                episodes,
+                np.nanmin(episodes_agg, axis=1), np.nanmax(episodes_agg, axis=1),
+                color='C1', alpha=0.1)
+        
+        plt.xlabel("Episode")
+        plt.ylabel("Total episode reward")
+        plt.legend(loc="lower right")
+        plt.title(f"{env_name}")
+        plt.savefig(f"{env_name}.jpg")
