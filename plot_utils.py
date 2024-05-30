67ebe1dc93d3385e21d2d8d84e01c95ce1670ac2
Bastien Marconato <bastien.marconato@icloud.com>
2024-05-30 17:17:58 +0200

    final

--- plot_utils.py
+++ plot_utils.py
@@ -6,7 +6,7 @@ def aggregate_over_seeds(results):
     max_timesteps = np.max([len(seed_data[0]) for seed_data in results])
     max_episodes = np.max([len(seed_data[1]) for seed_data in results])
     max_grad_steps = np.max([len(seed_data[2]) for seed_data in results])
-    
+
     reward_agg = np.empty((max_timesteps, len(results)))    # total reward
     reward_agg[:] = np.nan
 
@@ -21,7 +21,7 @@ def aggregate_over_seeds(results):
 
 
     for seed, seed_data in enumerate(results):
-        for t in range(max_timesteps): 
+        for t in range(max_timesteps):
             try:
                 reward_agg[t, seed] = seed_data[0][t]
             except IndexError:
@@ -50,7 +50,7 @@ def aggregate_over_seeds(results):
 
 def plot_results(results, env_name: str):
     env_result = results[env_name]
-    
+
     if isinstance(env_result, list):
         reward_agg, episodes_agg, critic_losses, actor_losses = aggregate_over_seeds(env_result)
         rewards_t = np.nanmean(reward_agg, axis=1)
@@ -86,43 +86,40 @@ def plot_results(results, env_name: str):
 
 if __name__ == '__main__':
     continuous_envs = ["MountainCarContinuous-v0", "Pendulum-v1"]
+    plt.style.use("seaborn-v0_8-poster")
     for env_name in continuous_envs:
-        plt.figure(figsize=(20,6))
-
         with open(f"sac_{env_name}.obj", 'rb') as f:
             sac_result = pickle.load(f)
-            
-            reward_agg, episodes_agg, critic_losses, actor_losses = aggregate_over_seeds(sac_result)
-
-            episodes = np.arange(0, episodes_agg.shape[0])
-            r_ep_mean = np.nanmean(episodes_agg, axis=1)
-            plt.plot(r_ep_mean, '.-', label='SAC', color='C0')
-            plt.fill_between(episodes, r_ep_mean, color='C0', alpha=0.2)
-            """
-            plt.fill_between(
-                episodes,
-                np.nanmin(episodes_agg, axis=1), np.nanmax(episodes_agg, axis=1),
-                color='C0', alpha=0.1)
-            """
+            sac_reward_agg, sac_episodes_agg, _, _ = aggregate_over_seeds(sac_result)
 
         with open(f"td3_{env_name}.obj", 'rb') as f:
             td3_result = pickle.load(f)
+            td3_reward_agg, td3_episodes_agg, _, _ = aggregate_over_seeds(td3_result)
 
-            reward_agg, episodes_agg, critic_losses, actor_losses = aggregate_over_seeds(td3_result)
+        #episodes = np.arange(0, sac_reward_agg.shape[0])
+        sac_r_ep_mean = np.nanmean(sac_episodes_agg[:500], axis=1)
+        td3_r_ep_mean = np.nanmean(td3_episodes_agg[:500], axis=1)
 
-            episodes = np.arange(0, episodes_agg.shape[0])
-            r_ep_mean = np.nanmean(episodes_agg, axis=1)
-            plt.plot(r_ep_mean, '.-', label='TD3', color='C1')
-            plt.fill_between(episodes, r_ep_mean, color='C0', alpha=0.2)
-            """
-            plt.fill_between(
-                episodes,
-                np.nanmin(episodes_agg, axis=1), np.nanmax(episodes_agg, axis=1),
-                color='C1', alpha=0.1)
-            """
-        
-        plt.xlabel("Episode")
-        plt.ylabel("Total episode reward")
-        plt.legend(loc="lower right")
-        plt.title(f"{env_name}")
+        fig, ax = plt.subplots(1, 2, figsize=(20,6))
+        ax[0].plot(sac_r_ep_mean, '-', label='SAC', alpha=0.6)
+        ax[0].plot(td3_r_ep_mean, '-', label='TD3', alpha=0.6)
+        ax[0].set_xlabel("Episode")
+        ax[0].set_ylabel("Total episode reward")
+        ax[0].legend(loc="lower right")
+
+        sac_r_t_mean = np.nanmean(sac_reward_agg, axis=1)
+        td3_r_t_mean = np.nanmean(td3_reward_agg, axis=1)
+
+        ax[1].plot(sac_r_t_mean, '-', label='SAC', alpha=0.6)
+        ax[1].plot(td3_r_t_mean, '-', label='TD3', alpha=0.6)
+        ax[1].set_xlabel("Timestep")
+        ax[1].set_ylabel("Accumulated reward")
+        if env_name == 'Pendulum-v1':
+            ax[1].legend(loc="upper right")
+        else:
+            ax[1].legend(loc="upper left")
+
+        plt.suptitle(f"{env_name}")
+        plt.tight_layout()
         plt.savefig(f"{env_name}.jpg")
+        plt.close()
